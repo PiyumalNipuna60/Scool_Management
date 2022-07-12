e1192b02b4326a24e3b05aef3750870e99d4bb28
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-07-12 09:48:37 +0530

    Set Ijse Database

--- School_Management/src/db/DBConnection.java
+++ School_Management/src/db/DBConnection.java
@@ -11,7 +11,7 @@ public class DBConnection {
 
     private DBConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/thogakade", "root", "1234");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ijse", "root", "1234");
 
     }
 
