1ef5158725212709fd665329cdec84be3a40850b
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-07-12 10:08:05 +0530

    Save Customer Content

--- School_Management/src/controller/StudentDashBoardFormController.java
+++ School_Management/src/controller/StudentDashBoardFormController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -26,6 +27,18 @@ public class StudentDashBoardFormController {
     public TableColumn colOption;
 
     public void saveOnAction(ActionEvent actionEvent) {
+        Student s=new Student(
+                txtStudentId.getText(),txtStudentName.getText(),txtStudentEmail.getText(),txtStudentContact.getText(),txtStudentAddress.getText(),txtStudentNic.getText()
+        );
+        try {
+            if (CrudUtil.execute("INSERT INTO Student VALUES(?,?,?,?,?,?)", s.getsId(), s.getsName(), s.getsEmail(), s.getsContact(), s.getsAddress(), s.getsNic())) {
+                new Alert(Alert.AlertType.CONFIRMATION, "Save Customer!..").show();
+            } else {
+                new Alert(Alert.AlertType.CONFIRMATION, "Something went Wrong!..").show();
+            }
+        }catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public void UpdateOnAction(ActionEvent actionEvent) {
