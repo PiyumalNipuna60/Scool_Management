c5fecccbc476870c33f6dc6334059565c711effd
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-07-12 09:40:32 +0530

    Controller & UI Designer

---
+++ School_Management/src/controller/StudentDashBoardFormController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class StudentDashBoardFormController {
+    public TextField txtStudentId;
+    public TextField txtStudentAddress;
+    public TextField txtStudentContact;
+    public TextField txtStudentEmail;
+    public TextField txtStudentName;
+    public TextField txtStudentNic;
+    public TableView tblStudent;
+    public TableColumn colStudentId;
+    public TableColumn colEmail;
+    public TableColumn colSId;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colNic;
+    public TableColumn colOption;
+
+    public void saveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void UpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void DeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void SearchStudentOnACtion(ActionEvent actionEvent) {
+    }
+}
